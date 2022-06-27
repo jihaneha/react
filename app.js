cc979cd8ae1185fd9926e3f8c06d2069babca694
jihaneha <98841225+jihaneha@users.noreply.github.com>
2022-06-27 09:28:20 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// let n = 0;
+// function render() {
+//   const title = React.createElement(
+//     "h1",
+//     {},
+//     "bonjour tout le monde ",
+//     React.createElement("span", {}, n)
+//   );
+//   ReactDOM.render(title, document.getElementById("app"));
+// }
+// render();
+// window.setInterval(() => {
+//   n++;
+//   render();
+// }, 1000);
